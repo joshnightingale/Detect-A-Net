e8fe86a6acbf28aba406258a3207aa31c9f9138c
josh <josh@hi.is>
2023-05-12 08:38:48 +0000

    Adding comments

--- analyse_knowndetection.R
+++ analyse_knowndetection.R
@@ -1,35 +1,50 @@
 library(nimble)
 library(MCMCvis)
 
+set.seed(69)
+
+# model code
 cod <- nimbleCode({
+  # loop through origins and destinations
   for (ii in 1:NsiteA) {
     for (ij in 1:NsiteB) {
+      # each observation is related to detection probabilities
+      # at both origin and destination
       obs.dyad[ii, ij] ~ dbinom(dyad[ii, ij], prob = (detectA[ii]*detectB[ij]))
+      
+      # prior for number of movements - model is sensitive to this
       dyad[ii, ij] ~ dpois(40)
     }
   }
 })
 
+# run model providing observations of dyads and true detections
 mod <- nimbleModel(code=cod,
                    data=list(obs.dyad=dyad.obs, detectA=detectA, detectB=detectB),
                    constants = list(NsiteA=Nsite, NsiteB=Nsite)
 )
 
+# configure MCMC
 myMCMC <- buildMCMC(mod, monitors=c("dyad"))
 
+
+# compile code
 mod.comp <- compileNimble(mod, showCompilerOutput = T)
 comp <- compileNimble(myMCMC)
 
+# run model
+samps <- runMCMC(mcmc = comp, niter=5000, nburnin = 4000, nchains = 2)
 
-samps <- runMCMC(mcmc = comp, niter=2000, nburnin = 1000, nchains = 2)
-
-
+# check posteriors and traceplots
 MCMCsummary(samps)     
 MCMCtrace(samps)
 
-matrix(MCMCsummary(samps)$mean, nrow=5)
-
-plot(trans, MCMCsummary(samps)$mean)
+# correlation between true and estimated values
 cor.test(as.vector(trans), MCMCsummary(samps)$mean)
 
-plot(rank(trans), rank(MCMCsummary(samps)$mean), xlab = "True number", ylab="Estimated number")
+# plot
+plot(trans, MCMCsummary(samps)$mean,
+     xlab = "True number", ylab="Estimated number")
+text(x=47.5, y=35, labels=paste("cor = ", round(cor(as.vector(trans), MCMCsummary(samps)$mean), 2)))
+# abline(0, 1, lty=3)
+# text(50, 50, labels=expression("y=x"))
--- simulate_CH.R
+++ simulate_CH.R
@@ -1,38 +1,51 @@
 require(magrittr)
 
+set.seed(69)
+
 ### true states
 
+# number of individuals
 Nind = 1000
 Nsite = 5
 
-detectA <- runif(Nsite, 0.5, 0.9)
-detectB <- runif(Nsite, 0.5, 0.9)
+## detection probabilties
+# A = breeding, B = wintering
+detectA <- runif(Nsite, 0.1, 0.9)
+detectB <- runif(Nsite, 0.1, 0.9)
 
+# which sites does each individual use
 states <- data.frame(
   individual = 1:Nind,
   siteA = sample(1:Nsite, Nind, T),
   siteB = sample(1:Nsite, Nind, T)
 )
 
-
+# empty matrices to store results
 trans <- matrix(0, nrow=Nsite, ncol=Nsite)
 dyad.obs <- matrix(0, nrow=Nsite, ncol=Nsite)
 
 obsA <- obsB <- rep(0, Nsite)
 
+# loop through matrices filling observation histories
 ii <- 1
 for (ii in 1:Nind) {
+  ## observations
+  # sites
   states$obsA[ii] <- rbinom(n=1, size=1, prob=detectA[states$siteA[ii]])
   states$obsB[ii] <- rbinom(n=1, size=1, prob=detectB[states$siteB[ii]])
+  # dyadic connections
   states$obs.dyad[ii] <- states$obsA[ii] * states$obsB[ii]
   
-  trans[states$siteA[ii], states$siteB[ii]] %<>% add(1)
-  dyad.obs[states$siteA[ii], states$siteB[ii]] %<>% add(states$obs.dyad[ii])
-  
+  # extract totals
   obsA[states$siteA[ii]] %<>% add(states$obsA[ii])
   obsB[states$siteB[ii]] %<>% add(states$obsB[ii])
+  dyad.obs[states$siteA[ii], states$siteB[ii]] %<>% add(states$obs.dyad[ii])
+  
+  ## true number of movements
+  trans[states$siteA[ii], states$siteB[ii]] %<>% add(1)
 }
 
+# plot relationships
 plot(dyad.obs, trans)
 plot(dyad.obs, trans*detectA)
 plot(dyad.obs, trans*detectB)
